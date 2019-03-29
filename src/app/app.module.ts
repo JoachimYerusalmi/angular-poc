9f7f858972d1825d2534c9e0c6ea8833915cad81
Yerusalmi, Joachim <JoachimYERUSALMI@eaton.com>
2019-03-29 15:46:41 +0100

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule, MatToolbarModule} from '@angular/material';
+import { Link1Component } from './components/link1/link1.component';
+import { Link2Component } from './components/link2/link2.component';
+import {AppRoutingModule} from "./app-routing/app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    Link1Component,
+    Link2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
